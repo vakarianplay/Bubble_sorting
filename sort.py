f38f575f23045011e8ce64573da91f54e31a1aa8
Kostya Vakarian <vakarianplay@gmail.com>
2019-12-16 22:06:09 +0300

    sort.py

--- sort.py
+++ sort.py
@@ -13,8 +13,6 @@ def partition(arr, low, high):
     arr[i + 1], arr[high] = arr[high], arr[i + 1]
     return (i + 1)
 
-
-
 # arr[] --> Array to be sorted,
 # low --> Starting index,
 # high --> Ending index
