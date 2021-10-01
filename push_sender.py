5330e156bd9d0a0851596e2550f813a496215fa4
Kostya Vakarian <vakarianplay@gmail.com>
2021-10-01 11:17:09 +0300

    Update push_sender.py

--- push_sender.py
+++ push_sender.py
@@ -1,13 +1,17 @@
 from notify_run import Notify
 import requests
 
+#get weather data
 def wttr():
     url = 'http://wttr.in/?format=%l:+%c+%t\nSunrise:%S\nSunset:%s\nDusk:%d\n%m'
     response = requests.get(url)
     data = response.text
     return data
 
+#connect push channel
 notify = Notify(endpoint="https://notify.run/PUdK0BvYeMmYTMWE")
+#info about channel
 inf =notify.info()
+#send data
 notify.send(wttr())
 print (inf)
