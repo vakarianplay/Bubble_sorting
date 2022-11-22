3174ea6b565262331e9117bd99525b31245b1bd8
Kostya Vakarian <vakarianplay@gmail.com>
2022-11-22 23:13:51 +0300

    Add files via upload

---
+++ Openweathermap.py
@@ -0,0 +1,72 @@
+import re
+import sys
+
+appid ="03d92e1dcdf06982a35783083f52690a"
+
+import requests
+
+def get_wind_direction(deg):
+    l = ['С ','СВ',' В','ЮВ','Ю ','ЮЗ',' З','СЗ']
+    for i in range(0,8):
+        step = 45.
+        min = i*step - 45/2.
+        max = i*step + 45/2.
+        if i == 0 and deg > 360-45/2.:
+            deg = deg - 360
+        if deg >= min and deg <= max:
+            res = l[i]
+            break
+    return res
+
+# Проверка наличия в базе информации о нужном населенном пункте
+def get_city_id(s_city_name):
+    try:
+        res = requests.get("http://api.openweathermap.org/data/2.5/find",
+                     params={'q': s_city_name, 'type': 'like', 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+        data = res.json()
+        cities = ["{} ({})".format(d['name'], d['sys']['country'])
+                  for d in data['list']]
+        print("city:", cities)
+        city_id = data['list'][0]['id']
+        print('city_id=', city_id)
+    except Exception as e:
+        print("Exception (find):", e)
+        pass
+    assert isinstance(city_id, int)
+    return city_id
+
+# Запрос текущей погоды
+def request_current_weather(city_id):
+    try:
+        opwRes = requests.get("http://api.openweathermap.org/data/2.5/weather",
+                     params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+        opwData = opwRes.json()
+        caption = opwData['weather'][0]['description']
+        currentTemp = str(opwData['main']['temp']) + " (" + str(opwData['main']['feels_like']) + ")"
+        print(caption)
+        print(currentTemp)
+    except Exception as e:
+        print("Exception (weather):", e)
+        pass
+
+# Прогноз
+def request_forecast(city_id):
+    try:
+        res = requests.get("http://api.openweathermap.org/data/2.5/forecast",
+                           params={'id': city_id, 'units': 'metric', 'lang': 'ru', 'APPID': appid})
+        data = res.json()
+        print('city:', data['city']['name'], data['city']['country'])
+        for i in data['list']:
+            print( (i['dt_txt'])[:16], '{0:+3.0f}'.format(i['main']['temp']),
+                   '{0:2.0f}'.format(i['wind']['speed']) + " м/с",
+                   get_wind_direction(i['wind']['deg']),
+                   i['weather'][0]['description'] )
+    except Exception as e:
+        print("Exception (forecast):", e)
+        pass
+
+#city_id for SPb
+# city_id = 563523
+
+request_current_weather(563523)
+request_forecast(563523)
